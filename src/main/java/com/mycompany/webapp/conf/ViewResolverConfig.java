77ebaf7c26240a9682d5c73e0c1b4ed922220a8a
gigomgigom <100766892+gigomgigom@users.noreply.github.com>
2024-05-30 11:20:08 +0900

    Create Rest-API for Front-End-Vue

---
+++ src/main/java/com/mycompany/webapp/conf/ViewResolverConfig.java
@@ -0,0 +1,23 @@
+package com.mycompany.webapp.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration //SpringBoot가 시작될때 사용되는 설정 객체로 사용
+public class ViewResolverConfig {
+	@Bean //리턴된 객체를 관리객체로 만들어주는 어노테이션
+	public ViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+}
